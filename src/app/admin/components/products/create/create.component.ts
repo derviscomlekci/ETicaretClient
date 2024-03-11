8beed2e93262c12983fcc993c3a2d42765baf05d
derviscomlekci <derviscomlek@gmail.com>
2024-03-12 01:33:20 +0300

    products create from admin ui

---
+++ src/app/admin/components/products/create/create.component.ts
@@ -0,0 +1,42 @@
+import { Create_Product } from './../../../../contracts/create_product';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../../services/common/models/product.service';
+import { BaseComponent, SpinnerType } from '../../../../base/base.component';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AlertifyService, MessagePosition, MessageType } from '../../../../services/admin/alertify.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.css'
+})
+export class CreateComponent extends BaseComponent implements OnInit {
+  constructor(spiner: NgxSpinnerService, private productService:ProductService,private alertify:AlertifyService){
+    super(spiner)
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  create(name:HTMLInputElement,stock:HTMLInputElement,price:HTMLInputElement){
+    this.showSpinner(SpinnerType.BallSpinClockwise);
+    const create_product:Create_Product=new Create_Product();
+    create_product.name=name.value;
+    create_product.stock=parseInt(stock.value);
+    create_product.price=parseFloat(price.value);
+
+    this.productService.create(create_product,()=>{
+      this.hideSpinner(SpinnerType.BallSpinClockwise)
+      this.alertify.message("Ürün basarili bir sekilde eklendi.",{
+        DismissOthers:true,
+        MessageType:MessageType.Success,
+        Position:MessagePosition.TopRight
+      })
+      
+    });
+  }
+
+  
+
+}
--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BaseComponent, SpinnerType } from '../../../base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { HttpClientService } from '../../../services/common/http-client.service';
-import { Product } from '../../../contracts/product';
+
 
 @Component({
   selector: 'app-admin-products',
@@ -20,58 +20,6 @@ export class ProductsComponent extends BaseComponent implements OnInit{
   }
   ngOnInit(): void {
     this.showSpinner(SpinnerType.Fire);
-
-
-    this.httpClientservice.get<Product[]>({
-      controller:"products"
-    }).subscribe(data=>console.log(data))
-
-
-    // this.httpClientservice.delete({
-    //   controller:"products"
-    // },"ece9853e-4c73-4647-95b5-1d1e368e8419").subscribe();
-
-    // this.httpClientservice.put({
-    //   controller:"products"
-    // },{
-    //   id:"20c352c3-9ff0-4424-9481-2b540cf54ed9",
-    //   name:"Kalemlik",
-    //   stock:101,
-    //   price:18
-    // }).subscribe();
-
-    // this.httpClientservice.post({
-    //   controller:"products"
-    // },{
-    //   name:"Kalem",
-    //   stock:100,
-    //   price:15
-    // }).subscribe();
-
-    // this.httpClientservice.post({
-    //   controller:"products"
-    // },{
-    //   name:"Silgi",
-    //   stock:1000,
-    //   price:1
-    // }).subscribe();
-
-    // this.httpClientservice.post({
-    //   controller:"products"
-    // },{
-    //   name:"Defter",
-    //   stock:1,
-    //   price:10
-    // }).subscribe();
-
-    // this.httpClientservice.post({
-    //   controller:"products"
-    // },{
-    //   name:"Tebsir",
-    //   stock:1200,
-    //   price:120
-    // }).subscribe();
-
 
   }
 
